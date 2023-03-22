3bb4092effcf3cd1ab77f384a4eac4fe5b43b275
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2023-03-22 15:58:24 +0530

    UI: added buttons {Completed, Delete, Update}

--- frontend/todo/src/components/TaskCard/TaskCard.js
+++ frontend/todo/src/components/TaskCard/TaskCard.js
@@ -6,10 +6,44 @@ class TaskCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: props.id,
       title: props.title,
       description: props.description,
       completed: props.completed
     };
+  }
+
+  onSubmitCompleted(event) {
+    event.preventDefault();
+    fetch('http://127.0.0.1:5000/task/' + this.state.id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        completed: true
+      })
+    });
+    this.setState({ completed: true });
+  }
+
+  onSubmitNotCompleted(event) {
+    event.preventDefault();
+    fetch('http://127.0.0.1:5000/task/' + this.state.id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        completed: false
+      })
+    });
+    this.setState({ completed: false });
+  }
+
+  onSubmitDelete(event) {
+    event.preventDefault();
+    fetch('http://127.0.0.1:5000/task/' + this.state.id, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    });
+    this.setState({ title: "", description: "", completed: false });
   }
 
   render() {
@@ -17,12 +51,19 @@ class TaskCard extends React.Component {
       <div className={styles.TaskCard}>
         <div className={styles.TaskFieldsGroup}>
           <div className={[styles.TaskTitle, styles.TaskField].join(' ')}>{this.state.title}</div>
-          <div className={[styles.TaskStatus, styles.TaskComplete].join(' ')}></div>
+          <div className={[styles.TaskStatus, (this.state.completed ? styles.TaskComplete : styles.TaskInComplete)].join(' ')}></div>
         </div>
         <div className={styles.TaskField}>
           {/* TODO: Smoothen the animation of text show */}
           <div className={styles.TaskDescription}>
             {this.state.description}
+          </div>
+        </div>
+        <div className={styles.TaskButtons}>
+          <div className={styles.TaskButtonsContainer}>
+            <button className={[styles.Btn, styles.PrimaryBtn].join(' ')} onClick={(e) => (this.state.completed) ? this.onSubmitNotCompleted(e) : this.onSubmitCompleted(e)}>{this.state.completed ? "NotCompleted" : "Completed"}</button>
+            <button className={[styles.Btn, styles.DangerBtn].join(' ')} onClick={(e) => this.onSubmitDelete(e)}>Delete</button>
+            <button className={[styles.Btn, styles.Warnbtn].join(' ')}>Update</button>
           </div>
         </div>
       </div>
--- frontend/todo/src/components/TaskList/TaskList.js
+++ frontend/todo/src/components/TaskList/TaskList.js
@@ -6,40 +6,28 @@ import TaskCard from './../TaskCard/TaskCard';
 class TaskList extends React.Component {
   constructor(props) {
     super(props);
-    this.SAMPLE_TITLE = "Title";
-    this.SAMPLE_DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed justo vel lacus commodo
-    suscipit sed vel nulla. Aliquam lacinia nisi non augue laoreet, eu molestie sapien hendrerit.
-    Vestibulum at ligula nec sapien molestie imperdiet. Suspendisse sit amet congue nisi.
-    Sed ultricies ante eget mauris interdum fringilla. Pellentesque habitant morbi tristique
-    senectus et netus et malesuada fames ac turpis egestas. Sed dapibus mi id lectus tempor, a
-    bibendum elit consequat. Vivamus dapibus tristique nunc, eget volutpat lorem pharetra sit amet.
-    Duis eu sagittis leo, non commodo velit. Mauris eget erat blandit, mollis neque vel, varius
-    tellus. Etiam ornare lobortis sapien, ac accumsan urna. Donec id nisl odio. Aliquam erat
-    volutpat.`;
-    this.tasks = {};
+    this.state = {
+      tasks: [],
+      DataisLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/task")
+      .then((response) => response.json())
+      .then((data) => { this.setState({ tasks: data, DataisLoaded: true }); });
   }
 
   render() {
-    // fetch('http://127.0.0.1:5000/task')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     data.forEach((e) => this.tasks.append(e));
-    //   }).then(console.log(this.tasks));
-
+    const { DataisLoaded, tasks } = this.state;
+    if (!DataisLoaded) return <div><h3> Loading....</h3></div>;
     return (
       <div className={styles.TaskList}>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
-        <TaskCard title={this.SAMPLE_TITLE} description={this.SAMPLE_DESCRIPTION}></TaskCard>
+        {
+          this.state.tasks.map(
+            (task) => <TaskCard key={task.id} id={task.id} title={task.title} description={task.description} completed={task.completed} />
+          )
+        }
       </div>
     );
   }
